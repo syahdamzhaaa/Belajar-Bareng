becdde21ec19f04040456b1cab5c6aa75a848fd9
Nafisyaaa <syahdamfacc12@gmail.com>
2024-04-28 19:37:15 +0700

    TEST 1

---
+++ Fungsi_FindWords1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool FindWords (const string* matriks, int JumlahBaris, int JumlahKolom, const string& Word) {
+    int WordLength = Word.length();
+    
+    cout<<"Percobaan 1";
+}
